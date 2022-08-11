dca1b06eca98642d4e84aaa3536326d389842f0c
minus-hunt <luchkintx@gmail.com>
2022-08-12 02:16:34 +0300

    Started learn course of JS by Vladilen

---
+++ JavaScript/app.js
@@ -0,0 +1,52 @@
+// 1 - Переменные
+// variable
+// const firstName = 'Maxim'
+// const lastName = 'Bratkrayts' // string
+// // const age = 25 // number
+// const isProgrammer = true // boolean
+// const _private = 'private'
+//
+
+// 2 - Работа с типами данных Мутирование
+// console.log('Your name: ' + firstName + ' ' + lastName + ' your age ' + age)
+// alert('Your name: ' + firstName + ' ' + lastName + ' your age ' + age)
+
+// const someMessage = prompt('Введите что то: ')
+// console.log(someMessage)
+
+// // 3 - Операторы
+// const currentYear = 2022
+// const birthYear = 1996
+//
+// let c = 32
+
+// const age = currentYear - birthYear
+// let a = 10
+// const b = 5
+
+// c = c + a
+// c += a
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a++)
+// console.log(a)
+
+// 4 - Типы данных
+// const isProgrammer = true
+// const name = 'Maxim'
+// const age = 25
+// let x, a, c
+//
+// console.log(typeof isProgrammer) // boolean
+// console.log(typeof name) // string
+// console.log(typeof age) // number
+// console.log(typeof x) // undefined
+// console.log(typeof null) // null
+//
+
+
+// 5 - Приоритет операторов
+fullAge = 25
+console.log(fullAge)
